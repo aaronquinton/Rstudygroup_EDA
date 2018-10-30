c456ee6609c532e78ba2b8a51f53a56c0fe7860b
Aaron Quinton <aaron.wquinton@gmail.com>
2018-10-30 00:43:24 -0700

    Added plots and slides

--- Avacado_EDA.R
+++ Avacado_EDA.R
@@ -11,55 +11,96 @@ library(lubridate)
 # Getting Started:
 
 # Load data set
-Avacado_Data <- read_csv("./Data/avocado.csv")
+df <- read_csv("https://raw.githubusercontent.com/aaronquinton/Rstudygroup_EDA/master/Data/avocado.csv")
 
 
 # Let's look at our data:
 
-Avacado_Data
-View(Avacado_Data)
+df
+str(df)
 
-head(Avacado_Data)
-tail(Avacado_Data)
+#Go back and change to read_csv
 
-str(Avacado_Data)
+glimpse(df)
 
-summary(Avacado_Data)
+View(df)
+
+summary(df)
+
 
 #equivalent to unique.data.frame, but much faster
-distinct(Avacado_Data)
+distinct(df)
 
-unique(Avacado_Data$type)
-unique(Avacado_Data$region)
-
-
-# Lets import our data again as df2 but using read.csv to compare
-
-df2 <- read.csv("./Data/avocado.csv")
-
-df2
-View(df2)
-
-str(df2)
+unique(df$type)
+unique(df$region)
 
 
 # Let's make the columns names more machine readable and select only the variables we want to look at.
 
-df_test <- Avacado_Data
+df_test <- df
 
 colnames(df_test) <- c("sort_id", "Avg_Price", "Tot_Vol")
 
-Avacado_Data <- rename(Avacado_Data, sort_id = X1)
+df <- rename(df, sort_id = X1)
 
-Avacado_Data <- select(Avacado_Data, date = Date, avg_price_usd = AveragePrice, 
+df <- select(df, date = Date, avg_price_usd = AveragePrice, 
              tot_vol = `Total Volume`, tot_bags = `Total Bags`, region, type)
 
-Avacado_Data <- Avacado_Data %>% 
-  mutate(type = factor(type), region = factor(region), date = ymd(date))
 
-str(Avacado_Data)
+# Lets look at the avg Price
+
+avg_price_plot <- df %>% 
+  ggplot(aes(x = avg_price_usd))
+    
+avg_price_plot +
+  geom_histogram()
+
+avg_price_plot +
+    geom_freqpoly()
+
+avg_price_plot  +
+    geom_density()
+
+avg_price_plot +
+    geom_boxplot()+
+    coord_flip()
+
+avg_price_plot +
+    stat_ecdf()
 
 
+# Lets add multiple variables
+# If its another categorical example
+
+avg_price_plot2 <- df %>% ggplot(aes(x = avg_price_usd, fill = type))
+ 
+avg_price_plot2 + geom_histogram()
+ 
+avg_price_plot2 + geom_histogram(position = "identity", alpha = 0.5)
+
+avg_price_plot2 + geom_freqpoly(aes(col = type))
+
+avg_price_plot2 + geom_density(alpha = 0.5)
+
+avg_price_plot + geom_boxplot(aes(x = type, y = avg_price_usd)) + coord_flip()
+
+avg_price_plot2 + stat_ecdf(aes(col = type)) 
+
+df %>% 
+  ggplot(aes(x = region, y = avg_price_usd)) +
+  geom_boxplot()  + coord_flip()
+
+df %>% 
+  ggplot(aes(y = avg_price_usd, x = region)) +
+    geom_quasirandom(aes(col = type), alpha = 0.5) +
+    coord_flip() +
+    guides(col = FALSE)
+
+df %>% 
+  ggplot(aes(y = avg_price_usd, x = region)) +
+  geom_quasirandom(aes(col = type), alpha = 0.5) +
+  coord_flip() +
+  guides(col = FALSE)
 
 
 
